66058d43f07bc803fa16d71abdd448dec665b8d4
ZaydQumsieh <zayd@qumsieh.net>
2017-02-18 14:03:32 -0800

    b

--- README.md
+++
@@ -1,4 +0,0 @@
-
-# WombatKombat
-
-A game made with the [n4](https://github.com/0xFireball/n4) game engine.
--- Sources/Registry.hx
+++
@@ -1,9 +0,0 @@
-package;
-
-import n4.NGame;
-
-import states.*;
-
-class Registry {
-	public static var PS:PlayState;
-}
